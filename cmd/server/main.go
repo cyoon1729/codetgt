d66033af05e3c8a3ba0026f53d17071af696ee45
cyoon1729 <chrisyoon1729@gmail.com>
2022-06-26 01:13:57 -0400

    some basic server init for now

---
+++ cmd/server/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"codetgt/internal/server"
+	"codetgt/internal/session"
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	fmt.Println("hello world!")
+
+	lobby := &server.Lobby{
+		Rooms:      make(map[string]map[*session.Room]bool),
+		Register:   make(chan session.Ticket),
+		Unregister: make(chan session.Ticket),
+	}
+
+	go lobby.Run()
+
+	router := gin.New()
+	router.LoadHTMLFiles("index.html")
+
+	router.GET("/room/:roomId", func(c *gin.Context) {
+		c.HTML(200, "index.html", nil)
+	})
+
+	router.GET("/ws/:roomId/:uuid/:name", func(c *gin.Context) {
+		usr := server.ConnInfo{
+			RoomId: c.Param("roomId"),
+			Uuid:   c.Param("uuid"),
+			Name:   c.Param("name"),
+		}
+		server.EnterRoom(lobby, c.Writer, c.Request, usr)
+	})
+
+	router.Run("0.0.0.0:8888")
+}

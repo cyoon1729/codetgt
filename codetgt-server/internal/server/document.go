d056d0a010ee93ebabdd21221e3c6bc7fda1ecb9
cyoon1729 <chrisyoon1729@gmail.com>
2022-07-03 02:08:17 -0400

    and here is the backend

---
+++ codetgt-server/internal/server/document.go
@@ -0,0 +1,17 @@
+package server
+
+type Document struct {
+	content    string
+	operations []int
+	language   string
+}
+
+func CreateEmptyDoc() *Document {
+	newDoc := &Document{
+		content:    "",
+		operations: make([]int, 0),
+		language:   "golang",
+	}
+
+	return newDoc
+}

60b11f4d54a1fd620b4bbbebfe699bea09b557e6
cyoon1729 <chrisyoon1729@gmail.com>
2022-07-04 02:23:37 -0400

    Use join room modal

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,18 +1,25 @@
 import {Container} from 'react-bootstrap';
 import './App.css';
+import { ChakraProvider } from '@chakra-ui/react'
 
 import CodeEditor from './CodeEditor';
+import Home from './Home';
+import JoinRoom from './JoinRoom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>
-        codetgt
-      </h1>
-      <Container>
-        <CodeEditor eid={1} />
-      </Container>
-    </div>
+    <ChakraProvider>          
+      <div className="App">
+        <h1>
+          codetgt
+        </h1>
+        <Home />
+        <JoinRoom />
+        <Container>
+          <CodeEditor eid={1} />
+        </Container>
+      </div>
+    </ChakraProvider>
   );
 }
 
